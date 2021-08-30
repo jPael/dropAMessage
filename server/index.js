d0e85a8fdfa4ca456bc4b255b7c1552608aeef31
jason pael <jasonjasonpael1234@gmail.com>
2021-08-31 01:25:58 +0800

    added more features

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-console.log("Starting");
+const http = require("http");
+const express = require("express");
+
+const app = express();
+
+const server = http.createServer(app);
+const port = 8080;
+app.use(express.static("public"));
+
+server.on("error", (err) => {
+    console.error("Server is having some problems: ", err);
+});
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
